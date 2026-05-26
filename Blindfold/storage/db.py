9f2afb27171ad7ef87ce2b4833bc8aaf92bafa2b
gux-htm <32safdar@gmail.com>
2026-05-26 18:12:09 +0500

    Fix SQLite threading connection error: add check_same_thread=False

--- Blindfold/storage/db.py
+++ Blindfold/storage/db.py
@@ -26,7 +26,7 @@ class EncryptedDatabase:
             except OSError:
                 pass
                 
-        self.conn = sqlite3.connect(self.db_path)
+        self.conn = sqlite3.connect(self.db_path, check_same_thread=False)
         self.conn.row_factory = sqlite3.Row
         self.chacha = ChaCha20Poly1305(self.key)
         self._init_db()
